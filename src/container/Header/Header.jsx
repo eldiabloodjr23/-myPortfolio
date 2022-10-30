e52f825f5ebe976f320546ab46b6ddc57c163897
eldiablood23 <moneyemoji.pro@gmai.com>
2022-10-31 00:36:31 +0100

    first commit

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -29,6 +29,7 @@ const Header = () => (
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
             <h1 className="head-text">Kamil</h1>
+            <p className="p-text">28 y.o from Prague 🌆 </p>
           </div>
         </div>
 
